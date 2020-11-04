e729638cd1746f11d145f8e7455427164468340d
ventuscoder <shivankcode@gmail.com>
2020-11-04 22:17:12 +0530

    Added number input feature and clear feature

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const expDisplay = document.querySelector('#display-ops');
 const eqDisplay = document.querySelector('#display-calc');
 const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', function(e){alert(button.textContent)}));
+const numBtns = document.querySelectorAll('.btn-number');
+const oprBtns = document.querySelectorAll('.btn-operator');
+const clearBtn = document.querySelector('#btn-clear');
+const plusMinBtn = document.querySelector('#btn-plus-min');
+const bckspcBtn = document.querySelector('#btn-back');
+const dotBtn = document.querySelector('#btn-dot');
+const msgBar = document.querySelector('.msg');
 
 let displayStore = {
     first: null,
@@ -13,3 +19,14 @@ let displayStore = {
     decimalUsed: false,
     answerReceived: false
 };
+
+function inputNum(num){
+    eqDisplay.textContent += num;
+}
+
+function clearDisplay() {
+   eqDisplay.textContent = ""; 
+}
+
+numBtns.forEach(btn => btn.addEventListener('click', () => eqDisplay.textContent.length <= 14 ? inputNum(btn.value) : msgBar.textContent = "Length cannot exceed 15 characters!"));
+clearBtn.addEventListener('click', clearDisplay);
